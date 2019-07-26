74e56384ea0fa2ac01ee490efb6a9f46035abdf0
xinheng.shao <shaoxinheng@gmail.com>
2019-07-26 11:52:18 +0800

    up

--- chapter05.process-api/18.exercise-08.c
+++ chapter05.process-api/18.exercise-08.c
@@ -1,42 +1,62 @@
 /**
  * @Answer 创建两个子进程，并使用pipe()系统调用，将一个
  * 子进程的标准输出连接到另一个子进程的标准输入。
+ * 
+ * 
+  int pipe(int fds[2]);
+  Parameters :
+  fd[0] will be the fd(file descriptor) for the 
+  read end of pipe.
+  fd[1] will be the fd for the write end of pipe.
+  Returns : 0 on Success.
+  -1 on error.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#define MSGSIZE 64
 
 int main()
 {
-  pid_t c_pid1 = fork();
-  pid_t c_pid2 = fork();
+
   int fd[2];
+  int nbytes;
+  char inbuf[MSGSIZE];
+  int status;
+  pid_t c_pid;
 
   if (pipe(fd) < 0)
   {
-    printf("pipe create error ");
-    return -1;
+    exit(1);
   }
-  if (c_pid1 == 0)
+
+  // Child process will write
+  c_pid = fork();
+  if (c_pid == 0)
   {
     close(fd[0]);
-    printf("Process 1(pid:%d)\n", getpid());
-    write(fd[1], "Hello, this is process 1\n", 12);
-    sleep(1);
+    printf("Child process is writing.\n");
+    char *msg = "Hello, this message is from child.";
+    write(fd[1], msg, MSGSIZE);
+    printf("Child process finished writing.\n");
   }
-  else if (c_pid2 == 0)
+  // Parent process will read
+  else if (c_pid > 0)
   {
+    wait(NULL);
     close(fd[1]);
-    char line[12];
-    int n = read(fd[0], line, 12);
-    printf("Process 2(pid:%d)\n", getpid());
-    write(STDOUT_FILENO, line, n); //子进程读取数据
-    sleep(2);
+    printf("Parent process is reading.The message is below:\n");
+    while ((nbytes = read(fd[0], inbuf, MSGSIZE)) > 0)
+    {
+      printf("%s\n", inbuf);
+    }
+    if (nbytes != 0)
+    {
+      exit(2);
+    }
+    printf("Parent process finished reading.\n");
   }
-  else if (c_pid1 > 0 && c_pid2 > 0)
-  {
-    printf("Parent process(pid:%d)\n", getpid());
-  }
+
   return 0;
 }
