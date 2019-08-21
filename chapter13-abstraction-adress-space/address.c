4ee4853a40d9f2e63bc6bf9f4c5fbcc91b0dae3a
xinheng.shao <shaoxinheng@gmail.com>
2019-08-21 15:00:00 +0800

    UP

--- chapter13-abstraction-adress-space/address.c
+++ chapter13-abstraction-adress-space/address.c
@@ -5,9 +5,10 @@ int MAX = 100;
 
 void hello()
 {
-  int x = 0;
+  void *x = malloc(sizeof(int));
+  void *y = malloc(sizeof(int));
   printf("location of function hello: %p\n", (void *)hello);
-  printf("location of x in hello: %p\n", (void *)&x);
+  printf("size of double in hello: %ld\n", (y - x));
 }
 
 int main()
@@ -17,7 +18,7 @@ int main()
   printf("location of code : %p\n", (void *)main);
   printf("location of heap : %p\n", (void *)malloc(2));
   int x = 3;
-  int y = 4;
+  double y = 4.0;
   int *z = malloc(sizeof(int));
   printf("location of stack: %p | %p | %p\n", &x, &y, z);
   return 0;
